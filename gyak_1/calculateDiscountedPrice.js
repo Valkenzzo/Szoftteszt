6e495ad31d5573d85bbdf6845929c1d2545d971a
Valko Attila Laszlo <valkoatti@hotmail.com>
2026-09-07 12:52:44 +0200

    Done

---
+++ gyak_1/calculateDiscountedPrice.js
@@ -0,0 +1,20 @@
+function calculateDiscountedPrice(orderValue, isVip) {
+    let discount = 0;
+
+    if (orderValue >= 10000 && orderValue < 25000) {
+        discount = 0.05;
+    } else if (orderValue >= 25000 && orderValue < 50000) {
+        discount = 0.10;
+    } else if (orderValue >= 50000) {
+        discount = 0.15;
+    }
+    if (isVip === true) {
+        discount += 0.05;
+    }
+
+    if (discount > 0.20) {
+        discount = 0.20;
+    }
+
+    return orderValue - (orderValue * discount);
+}
